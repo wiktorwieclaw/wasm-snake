6aaf1cdb9921cc9b6e5d25b5f88843c334b5561b
wwieclaw <lonevixu@gmail.com>
2021-02-17 00:08:41 +0100

    added Canvas and Input modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
+mod canvas;
+mod input;
+mod utils;
+
 use wasm_bindgen::prelude::*;
-use web_sys::HtmlCanvasElement;
-use web_sys::CanvasRenderingContext2d;
 use wasm_bindgen::{closure::Closure, JsCast, JsValue};
-use std::f64;
 use std::rc::Rc;
 use std::cell::RefCell;
+use web_sys::HtmlCanvasElement;
+
+use crate::canvas::Canvas;
+use crate::input::Input;
+use crate::utils::{Position, Size};
 
 fn request_animation_frame(f: &Closure<dyn FnMut()>) {
     web_sys::window()
@@ -16,68 +22,57 @@ fn request_animation_frame(f: &Closure<dyn FnMut()>) {
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
     let window = web_sys::window().unwrap();
-
-    let document = window
-        .document()
-        .unwrap();
-
+    let document = window.document().unwrap();
     let body = document.body().unwrap();
-
-    let header = document.create_element("p")?;
-    header.set_inner_html("Hello from Rust!");
-
-    body.append_child(&header)?;
-
+    let header = document.create_element("h1")?;
     let canvas = document
         .create_element("canvas")?
         .dyn_into::<HtmlCanvasElement>()?;
 
-    canvas.set_width(300);
-    canvas.set_height(300);
+    header.set_inner_html("wasm-snake");
 
+    body.append_child(&header)?;
     body.append_child(&canvas)?;
 
-    let context: CanvasRenderingContext2d = canvas
-        .get_context("2d")?
-        .unwrap()
-        .dyn_into::<CanvasRenderingContext2d>()?;
+    let mut canvas = Canvas::from(canvas)?;
+    canvas.set_size(Size { w: 500, h: 500 });
 
-    context.begin_path();
+    let input = Input::new();
+    input.connect_to_canvas(&canvas)?;
 
     let f = Rc::new(RefCell::new(None));
     let g = f.clone();
 
-    let (mut x, mut y) = (10.0, 10.0);
-
-    enum Direction {
-        Left,
-        Right
-    }
-    let mut direction = Direction::Right;
+    let mut pos: Position = Position { x: 0, y: 0 };
 
     *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
-        context.set_fill_style(&JsValue::from_str("white"));
-        context.fill_rect(0.0, 0.0, canvas.width() as f64, canvas.height() as f64);
-
-        context.set_fill_style(&JsValue::from_str("black"));
-        context.fill_rect(x, y, 10.0, 10.0);
-
-        if x > canvas.width() as f64 - 10.0 {
-            direction = Direction::Left;
-        } else if x < 0.0 {
-            direction = Direction::Right;
+        if input.is_pressed("ArrowUp") {
+            pos.y -= 1;
         }
 
-        match direction {
-            Direction::Right => {
-                x += 1.0;
-                y += 1.0;
-            },
-            Direction::Left => {
-                x -= 1.0;
-                y -= 1.0;
-            }
+        if input.is_pressed("ArrowDown") {
+            pos.y += 1;
         }
+
+        if input.is_pressed("ArrowLeft") {
+            pos.x -= 1;
+        }
+
+        if input.is_pressed("ArrowRight") {
+            pos.x += 1;
+        }
+
+        canvas.draw_rect(
+            Position { x: 0, y: 0 },
+            Size { w: canvas.width(), h: canvas.height() },
+            "white",
+        );
+
+        canvas.draw_rect(
+            Position { x: pos.x, y: pos.y },
+            Size { w: 10, h: 10 },
+            "black",
+        );
 
         request_animation_frame(f.borrow().as_ref().unwrap());
     }) as Box<dyn FnMut()>));
